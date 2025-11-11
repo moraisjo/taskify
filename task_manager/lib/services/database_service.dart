d185b8ef497c8158ae4e32101c40c5e2d15dfb17
Joana Morais <joanaalvesm@gmail.com>
2025-11-10 22:03:08 -0300

    feat: persistência p linux desktop

--- task_manager/lib/services/database_service.dart
+++ task_manager/lib/services/database_service.dart
@@ -1,6 +1,9 @@
+
+import 'dart:io';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 import '../models/task.dart';
 
@@ -17,9 +20,13 @@ class DatabaseService {
   }
 
   Future<Database> _initDB(String fileName) async {
+    // Detect desktop (Linux, Windows, macOS)
+    if (Platform.isLinux || Platform.isWindows || Platform.isMacOS) {
+      sqfliteFfiInit();
+      databaseFactory = databaseFactoryFfi;
+    }
     final directory = await getApplicationDocumentsDirectory();
     final path = join(directory.path, fileName);
-
     return await openDatabase(
       path,
       version: 1,
